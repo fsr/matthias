723553e9ea9f789d45d43c184332124d3b07ef79
Kilian Koeltzsch <me@kilian.io>
2016-09-22 22:19:17 +0200

    refactor googleimages to new format

--- plugins/googleimages/googleimages.go
+++ plugins/googleimages/googleimages.go
@@ -7,29 +7,30 @@ import (
 	"github.com/abourget/slick"
 )
 
-// GoogleImages searching
-// Commands:
-// !image (me) <searchterm> - Returns link to a random image result
-type GoogleImages struct{}
+type googleimages struct{}
 
 func init() {
-	slick.RegisterPlugin(&GoogleImages{})
+	slick.RegisterPlugin(&googleimages{})
 }
 
 // InitPlugin ...
-func (images *GoogleImages) InitPlugin(bot *slick.Bot) {
+func (images *googleimages) InitPlugin(bot *slick.Bot) {
 	bot.Listen(&slick.Listener{
 		Matches:            regexp.MustCompile("^!image( me)? (.*)"),
-		MessageHandlerFunc: images.ImageHandler,
+		MessageHandlerFunc: images.imageHandler,
 	})
 }
 
-// ImageHandler ...
-func (images *GoogleImages) ImageHandler(listen *slick.Listener, msg *slick.Message) {
+func (images *googleimages) imageHandler(listen *slick.Listener, msg *slick.Message) {
 	query := msg.Match[2]
 	log.Println("Google Image search for", query, "requested by", msg.FromUser.Name)
-	msg.Reply(`Sorry, das Feature funktioniert noch nicht ganz :/
+	msg.Reply(`Sorry, das Feature funktioniert noch nicht ganz :confused:
 Wenn du helfen magst es einzubauen, schau doch bitte mal hier vorbei: https://github.com/google/google-api-go-client
 Hier ist die Doku zu dem Custom Search Gedöns: https://godoc.org/google.golang.org/api/customsearch/v1
-Hatte beim Schreiben dieser Nachricht aktuell keine Lust mich da einzulesen. Wenn du Lust dazu hast, nur zu :)`)
+Hatte beim Schreiben dieser Nachricht aktuell keine Lust mich da einzulesen. Wenn du Lust dazu hast, nur zu :smile:
+Solltest du ein alternatives golang Package findest, was das schon implementiert, wär das natürlich auch klasse :blush:`)
+}
+
+func (images *googleimages) String() string {
+	return `!image (me) <suchbegriff> - Gibt ein Zufallsbild der Google Bildersuche zum <suchbegriff> aus`
 }
