c8adafa77a1a1f6b980d1649dd249e22d2d24ca3
Kilian Koeltzsch <me@kilian.io>
2016-09-22 22:27:29 +0200

    refactor random to new format

--- plugins/random/random.go
+++ plugins/random/random.go
@@ -10,41 +10,40 @@ import (
 	"github.com/abourget/slick"
 )
 
-// Random stuff not fitting in anywhere else.
-// Commands:
-// !bash - Return a random bash quote link
-type Random struct{}
+type random struct{}
 
 func init() {
-	slick.RegisterPlugin(&Random{})
+	slick.RegisterPlugin(&random{})
 }
 
 // InitPlugin ...
-func (random *Random) InitPlugin(bot *slick.Bot) {
+func (random *random) InitPlugin(bot *slick.Bot) {
 	bot.Listen(&slick.Listener{
 		Matches:            regexp.MustCompile("^!bash"),
-		MessageHandlerFunc: random.BashHandler,
+		MessageHandlerFunc: random.bashHandler,
 	})
 	bot.Listen(&slick.Listener{
 		Matches:            regexp.MustCompile("matthias ist (.*)"),
-		MessageHandlerFunc: random.InsultHandler,
+		MessageHandlerFunc: random.insultHandler,
 	})
 	bot.Listen(&slick.Listener{
 		Matches:            regexp.MustCompile("matthias,? du bist (.*)"),
-		MessageHandlerFunc: random.InsultHandler,
+		MessageHandlerFunc: random.insultHandler,
 	})
 }
 
-// BashHandler ...
-func (random *Random) BashHandler(listen *slick.Listener, msg *slick.Message) {
+func (random *random) bashHandler(listen *slick.Listener, msg *slick.Message) {
 	log.Println("Bash Quote requested by", msg.FromUser.Name)
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	randomID := r.Intn(950)
 	msg.Reply(fmt.Sprintf("http://bash.fsrleaks.de/?%d", randomID))
 }
 
-// InsultHandler ...
-func (random *Random) InsultHandler(listen *slick.Listener, msg *slick.Message) {
+func (random *random) insultHandler(listen *slick.Listener, msg *slick.Message) {
 	log.Println(msg.FromUser.Name, "insulted matthias with", msg.Match[1])
 	msg.ReplyMention(fmt.Sprintf("Deine Mudda ist %s!", msg.Match[1]))
+}
+
+func (random *random) String() string {
+	return `!bash - Link zu einem zufälligen Bashzitat`
 }
