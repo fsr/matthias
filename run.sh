5ffd46968fbd338bce570ac78eacf12605d0ca5b
Kilian Koeltzsch <me@kilian.io>
2018-01-22 11:06:59 +0100

    remove unecessary stuff & add run.sh

---
+++ run.sh
@@ -0,0 +1,12 @@
+docker run -d --name matthias \
+		-e FSRUSERNAME='' \
+		-e FSRPASSWORD='' \
+		-e HUBOT_ALIAS='!' \
+		-e HUBOT_FORECAST_API_KEY='' \
+		-e HUBOT_WEATHER_CELSIUS='1' \
+		-e HUBOT_GOOGLE_CSE_ID='' \
+		-e HUBOT_GOOGLE_CSE_KEY='' \
+		-e HUBOT_SLACK_TOKEN='' \
+		-e HUBOT_MAINTAINERS='shell,kilian' \
+		matthias -a slack
+
